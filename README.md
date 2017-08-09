1f7f27102f05fdfc18c0f57b38b3617fac1c8aba
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-08-09 16:27:04 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cash Flow Calculation   
+
+Cash flow calculation table can be used for loan amortization. It's written in AngularJS.
--- controllers/loanController.js
+++ controllers/loanController.js
@@ -15,20 +15,21 @@ var app = angular.module('app', []);
       vm.monthlyPayment = payment.toFixed(2);
 
       for (var i=1; i<=month; i++) {
-        var interest = principal * rate;        
-        var principalPayment = payment - interest;        
+        var interestPayment = principal * rate;        
+        var principalPayment = payment - interestPayment;        
         principal = principal - principalPayment;
 
         var periodicPayment = {
-          paymentNumber: i,
-          principalPayment: principalPayment.toFixed(2),
-          interest: interest.toFixed(2),
-          principal: principal.toFixed(2)
+          "paymentNumber" : i,
+          "principalPayment" : principalPayment.toFixed(2),
+          "interest" : interestPayment.toFixed(2),
+          "principal" : principal.toFixed(2)
         };
 
         vm.loanTable.push(periodicPayment);
       };
     };
-  }
+  };
+
   app.controller('LoanController', loanFunct);
 })();
