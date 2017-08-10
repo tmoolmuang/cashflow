98f4c4f1bada52e010e3d795f81e21f04be62822
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-08-10 12:31:58 -0700

    allow multiple loans

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -3,16 +3,60 @@ var app = angular.module('app', []);
 (function() {
   function loanFunct() {
     var vm = this;
+    var loanTable = [];
+    var count;
+    var longestTerm = 0;
+    
     vm.loanTable = [];
-
+    vm.loanTableArray = [];
+    vm.poolTable = [];
+    
+    vm.getLoanCount = function() {
+      count = parseInt(vm.loanCount);
+      vm.loanCountArray = new Array(parseInt(count)); 
+     };
+  
     vm.calculate = function() {
-      vm.loanTable.length = 0;
+      vm.loanTableArray.length = 0;
+      for (var i=0; i<count; i++) {
+        if (parseInt(vm.term[i]) > longestTerm) {
+          longestTerm = vm.term[i];
+        }
+        vm.loanTableArray.push(calculateLoan(vm.amount[i], vm.term[i], vm.rate[i]).concat());
+      }
+      calculatePool();
+    };
+
+    var calculatePool = function() {
+      vm.poolTable.length = 0;
+
+      for (var i=1; i<=longestTerm; i++) {
+        var periodicPayment = {
+          "paymentNumber" : i,
+          "principalPayment" : parseFloat(0),
+          "interest" : parseFloat(0),
+          "principal" : parseFloat(0)
+        };
+        vm.poolTable.push(periodicPayment);
+      };
+
+      for (var i=0; i<vm.loanTableArray.length; i++) {
+        var table = vm.loanTableArray[i];
+        for (var j=0; j<table.length; j++) {
+          vm.poolTable[j].principalPayment += parseFloat(table[j].principalPayment);
+          vm.poolTable[j].interest += parseFloat(table[j].interest);
+          vm.poolTable[j].principal += parseFloat(table[j].principal);
+        }
+      }
+    }
+    
+    var calculateLoan = function(p, m, r) {
+      loanTable.length = 0;
       
-      var principal = parseFloat(vm.amount);
-      var month = parseInt(vm.term);
-      var rate = parseFloat(vm.rate)/1200;
+      var principal = parseFloat(p);
+      var month = parseInt(m);
+      var rate = parseFloat(r)/1200;
       var payment = principal*(rate+(rate/(Math.pow(1+rate, month)-1)));
-      vm.monthlyPayment = payment.toFixed(2);
 
       for (var i=1; i<=month; i++) {
         var interestPayment = principal * rate;        
@@ -26,8 +70,9 @@ var app = angular.module('app', []);
           "principal" : principal.toFixed(2)
         };
 
-        vm.loanTable.push(periodicPayment);
+        loanTable.push(periodicPayment);
       };
+      return loanTable;
     };
   };
 
